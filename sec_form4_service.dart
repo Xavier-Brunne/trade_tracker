84a6f1d13935b89fc4424d8e8e62ad042a5bc3d6
Darren Brunne <153030875+Xavier-Brunne@users.noreply.github.com>
2025-11-03 17:44:14 +1000

    Add files via upload

---
+++ sec_form4_service.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class SecForm4Service {
+  final String baseUrl = 'https://data.sec.gov/submissions/';
+
+  Future<List<Map<String, dynamic>>> fetchForm4Filings(String cik) async {
+    final url = Uri.parse('$baseUrl' + 'CIK$cik.json');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final filings = data['filings']['recent'];
+      final form4Indexes = List.generate(filings['form'].length, (i) => i)
+          .where((i) => filings['form'][i] == '4')
+          .toList();
+
+      return form4Indexes.map((i) {
+        return {
+          'accessionNumber': filings['accessionNumber'][i],
+          'filingDate': filings['filingDate'][i],
+          'reportDate': filings['reportDate'][i],
+          'issuer': data['name'],
+        };
+      }).toList();
+    } else {
+      throw Exception('Failed to load Form 4 filings');
+    }
+  }
+}
