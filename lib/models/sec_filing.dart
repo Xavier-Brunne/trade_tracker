91d08d3e96fe644a73a13b9b8803d3258d728f73
Darren Brunne <153030875+Xavier-Brunne@users.noreply.github.com>
2025-11-04 08:48:38 +1000

    Update sec_filing.dart

--- lib/models/sec_filing.dart
+++ lib/models/sec_filing.dart
@@ -1,4 +1,52 @@
 
+import 'package:hive/hive.dart';
+
+part 'sec_filing.g.dart';
+
+@HiveType(typeId: 0)
+class SecFiling extends HiveObject {
+  @HiveField(0)
+  final String accessionNumber;
+
+  @HiveField(1)
+  final String filingDate;
+
+  @HiveField(2)
+  final String reportDate;
+
+  @HiveField(3)
+  final String issuer;
+
+  @HiveField(4)
+  bool isSaved;
+
+  SecFiling({
+    required this.accessionNumber,
+    required this.filingDate,
+    required this.reportDate,
+    required this.issuer,
+    this.isSaved = false,
+  });
+
+  factory SecFiling.fromJson(Map<String, dynamic> json) {
+    return SecFiling(
+      accessionNumber: json['accessionNumber'],
+      filingDate: json['filingDate'],
+      reportDate: json['reportDate'],
+      issuer: json['issuer'],
+      isSaved: json['isSaved'] ?? false,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'accessionNumber': accessionNumber,
+        'filingDate': filingDate,
+        'reportDate': reportDate,
+        'issuer': issuer,
+        'isSaved': isSaved,
+      };
+}
+
 class SecFiling {
   final String accessionNumber;
   final String filingDate;
